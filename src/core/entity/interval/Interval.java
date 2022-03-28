88b99fa24913991e67fb41b48777927a69ebfc24
David.Amateau <David.Amateau@bottomline.com>
2022-03-28 16:17:32 -0400

    base entities

--- src/core/entity/interval/Interval.java
+++ src/core/entity/interval/Interval.java
@@ -2,55 +2,28 @@ package core.entity.interval;
 
 import core.entity.Note;
 import core.entity.key.Key;
-import core.entity.pitch.Pitch;
-import java.util.HashMap;
-import java.util.List;
 
-import static core.entity.interval.IntervalUtils.*;
 
 public class Interval {
 
-  public static final HashMap<IntervalStep, IntervalQuality> INTERVAL_QUALITIES_MAP =
-      loadIntervalQualities();
-  public static final List<IntervalStep> CONSONANCES = loadConsonantIntervals();
-  public static final List<IntervalStep> DISSONANCES = loadDissonantIntervals();
+    private final Note noteVoice1;
+    private final Note noteVoice2;
+    private final Key key;
+    private final IntervalQuality quality;
 
-  private final Note lowVoice;
-  private final Note highVoice;
-  private final IntervalStep step;
-  private final IntervalQuality quality;
-  private final Key key;
+    public Interval(Note noteVoice1, Note noteVoice2, Key key) {
+        this.noteVoice1 = noteVoice1;
+        this.noteVoice2 = noteVoice2;
+        this.key = key;
+        this.quality = findQuality();
+    }
 
-  public Interval(Note lowVoice, Note highVoice, Key key) {
-    this.lowVoice = lowVoice;
-    this.highVoice = highVoice;
-    this.key = key;
-    this.step = findStep(lowVoice, highVoice);
-    this.quality = findQuality(step);
-  }
+    private IntervalQuality findQuality() {
+        int distanceInKey = 0;
+        int distanceInSemitones = Math.abs(
+                this.noteVoice1.getPitch().getNoteNumber() - this.noteVoice2.getPitch().getNoteNumber());
 
-  // TODO
-  private IntervalStep findStep(Note lowNote, Note highNote) {
-    int differenceInSemiTones = lowNote.compareTo(highNote);
-    Pitch lowNotePitch = lowNote.getPitch();
-    Pitch highNotePitch = highNote.getPitch();
-
-    return null;
-  }
-
-  public boolean isConsonance() {
-    return CONSONANCES.contains(step);
-  }
-
-  public boolean isDissonant() {
-    return DISSONANCES.contains(step);
-  }
-
-  public boolean isTritone() {
-    return step.getStepInSemitones() == 6;
-  }
-
-  public static IntervalQuality findQuality(IntervalStep step) {
-    return INTERVAL_QUALITIES_MAP.get(step);
-  }
+        // TODO
+        return null;
+    }
 }
