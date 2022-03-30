4448f7f7600aa9075e62b43781ab12e593d5a14e
David Amateau <damateau@gmail.com>
2022-03-29 20:33:58 -0400

    base entities

--- src/core/entity/interval/IntervalUtils.java
+++ src/core/entity/interval/IntervalUtils.java
@@ -1,31 +1,33 @@
 package core.entity.interval;
 
 import static core.entity.interval.IntervalQuality.*;
+import static core.entity.interval.IntervalType.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class IntervalUtils {
 
-//  static HashMap<IntervalQuality, IntervalQualityTemp> loadIntervalQualities() {
-//    return new HashMap<IntervalQuality, IntervalQualityTemp>() {
-//      {
-//        put(UNISON, PERFECT_CONSONANCE);
-//        put(MINOR_SECOND, DISSONANCE);
-//        put(MAJOR_SECOND, DISSONANCE);
-//        put(MINOR_THIRD, IMPERFECT_CONSONANCE);
-//        put(MAJOR_THIRD, IMPERFECT_CONSONANCE);
-//        put(PERFECT_FOURTH, DISSONANCE);
-//        put(AUGMENTED_FOURTH, DISSONANCE);
-//        put(DIMINISHED_FIFTH, DISSONANCE);
-//        put(PERFECT_FIFTH, PERFECT_CONSONANCE);
-//        put(MINOR_SIXTH, IMPERFECT_CONSONANCE);
-//        put(MAJOR_SIXTH, IMPERFECT_CONSONANCE);
-//        put(MINOR_SEVENTH, DISSONANCE);
-//        put(MAJOR_SEVENTH, DISSONANCE);
-//        put(OCTAVE, PERFECT_CONSONANCE);
-//      }
-//    };
-//  }
+  static HashMap<IntervalQuality, IntervalType> loadIntervalQualities() {
+    return new HashMap<IntervalQuality, IntervalType>() {
+      {
+        put(UNISON, PERFECT_CONSONANCE);
+        put(MINOR_SECOND, DISSONANCE);
+        put(MAJOR_SECOND, DISSONANCE);
+        put(MINOR_THIRD, IMPERFECT_CONSONANCE);
+        put(MAJOR_THIRD, IMPERFECT_CONSONANCE);
+        put(PERFECT_FOURTH, DISSONANCE);
+        put(AUGMENTED_FOURTH, DISSONANCE);
+        put(DIMINISHED_FIFTH, DISSONANCE);
+        put(PERFECT_FIFTH, PERFECT_CONSONANCE);
+        put(MINOR_SIXTH, IMPERFECT_CONSONANCE);
+        put(MAJOR_SIXTH, IMPERFECT_CONSONANCE);
+        put(MINOR_SEVENTH, DISSONANCE);
+        put(MAJOR_SEVENTH, DISSONANCE);
+        put(OCTAVE, PERFECT_CONSONANCE);
+      }
+    };
+  }
 
   static List<IntervalQuality> loadConsonantIntervals() {
     return List.of(
