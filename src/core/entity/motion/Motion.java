7be861e269f5037594338dc8d431cb017bdd0f61
David Amateau <damateau@gmail.com>
2022-04-07 01:41:33 -0400

    adding motion models

--- src/core/entity/motion/Motion.java
+++ src/core/entity/motion/Motion.java
@@ -1,6 +1,7 @@
 package core.entity.motion;
 
 import core.composition.Note;
+import core.entity.interval.Interval;
 import java.util.List;
 
 public class Motion {
@@ -13,6 +14,34 @@ public class Motion {
     this.voice1 = voice1;
     this.voice2 = voice2;
     this.motionType = findMotionType();
+  }
+
+  private MotionType findMotionType() {
+    MotionDirection voice1Direction = findMotionDirection(voice1.get(0), voice1.get(1));
+    MotionDirection voice2Direction = findMotionDirection(voice2.get(0), voice2.get(1));
+    Interval voice1Interval = new Interval(voice1.get(0).getPitch(), voice1.get(1).getPitch());
+    Interval voice2Interval = new Interval(voice2.get(0).getPitch(), voice2.get(1).getPitch());
+
+    if ((voice1Direction == MotionDirection.UP && voice2Direction == MotionDirection.UP)
+        || (voice1Direction == MotionDirection.DOWN && voice2Direction == MotionDirection.DOWN)) {
+      if (voice1Interval.findIntervalQuality().getStepInKey() == voice2Interval.findIntervalQuality().getStepInKey()) {
+        return MotionType.PARALLEL;
+      } else {
+        return MotionType.SIMILAR;
+      }
+    }
+
+    if ((voice1Direction == MotionDirection.UP && voice2Direction == MotionDirection.DOWN)
+        || (voice1Direction == MotionDirection.DOWN && voice2Direction == MotionDirection.UP)) {
+      return MotionType.CONTRARY;
+    }
+
+    if ((voice1Direction == MotionDirection.NONE && voice2Direction != MotionDirection.NONE)
+        || (voice1Direction != MotionDirection.NONE && voice2Direction == MotionDirection.NONE)) {
+      return MotionType.OBLIQUE;
+    }
+
+    return null;
   }
 
   public MotionDistance findMotionDistance(Note note1, Note note2) {
@@ -34,8 +63,4 @@ public class Motion {
     return MotionDirection.NONE;
   }
 
-  private MotionType findMotionType() {
-    // TODO
-    return null;
-  }
 }
