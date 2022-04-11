48925182e6aa31c448c573a4e512f987a5045c81
David Amateau <damateau@gmail.com>
2022-04-11 19:46:19 -0400

    random generators for enums

--- src/core/entity/key/ScaleDegree.java
+++ src/core/entity/key/ScaleDegree.java
@@ -1,5 +1,7 @@
 package core.entity.key;
 
+import java.util.Random;
+
 public enum ScaleDegree {
   TONIC,
   SUPER_TONIC,
@@ -7,5 +9,17 @@ public enum ScaleDegree {
   SUB_DOMINANT,
   DOMINANT,
   SUB_MEDIANT,
-  LEADING_TONE
+  LEADING_TONE;
+
+  public static ScaleDegree getRandomScaleDegree() {
+    return switch (new Random().nextInt(7)) {
+      case 0 -> TONIC;
+      case 1 -> SUPER_TONIC;
+      case 2 -> MEDIANT;
+      case 3 -> SUB_DOMINANT;
+      case 4 -> DOMINANT;
+      case 5 -> SUB_MEDIANT;
+      default -> LEADING_TONE;
+    };
+  }
 }
